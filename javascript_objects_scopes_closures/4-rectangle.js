745edd0d1a0cb20103c51352e9a6b1df116f24ed
John-Akech <j.akech@alustudent.com>
2023-09-28 06:34:00 +0000

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -5,9 +5,9 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    }
+   }
   }
-  print () {
+   print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
@@ -16,12 +16,12 @@ class Rectangle {
       console.log(s);
     }
   }
-  rotate () {
+   rotate () {
     const aux = this.width;
     this.width = this.height;
     this.height = aux;
   }
-  double () {
+   double () {
     this.width *= 2;
     this.height *= 2;
   }
